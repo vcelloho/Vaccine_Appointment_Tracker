5148c3277a1d711cafa5a4f53e0932a41f29b17f
vcelloho <vcelloho@gmail.com>
2021-03-01 19:47:46 -0700

    committing settings file with generic values

---
+++ settings.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  1 19:30:09 2021
+
+@author: Jonathan Ho
+"""
+
+
+TwitterConsumerKey = "CONSUMER KEY"
+TwitterConsumerSecret = "CONSUMER SECRET"
+TwitterAccessToken = "Access Token"
+TwitterAccessTokenSecret = "Access Token Secret"
+EmailAddress = "example@gmail.com"
+EmailPassword = "password"
+EmailSTMPAddress = "smtp.gmail.com"
+EmailSTMPPort = 587
